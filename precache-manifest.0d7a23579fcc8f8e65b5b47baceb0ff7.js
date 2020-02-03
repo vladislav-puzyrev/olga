c1259b1fd8eaa198eaee3a70906aabd9371a9de7
unknown <vladislav.puzzyrev@gmail.com>
2020-02-03 20:58:41 +0500

    Updates

---
+++ precache-manifest.0d7a23579fcc8f8e65b5b47baceb0ff7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3afa1c9c53ab19d7f09f5217d61fd21",
+    "url": "/olga/index.html"
+  },
+  {
+    "revision": "a2b84253bfb3ff4d33ba",
+    "url": "/olga/static/css/main.1e4fb050.chunk.css"
+  },
+  {
+    "revision": "247559a178dc354506db",
+    "url": "/olga/static/js/2.08b9fd1a.chunk.js"
+  },
+  {
+    "revision": "64be437c74fd47fbfe0ef57becb73f7b",
+    "url": "/olga/static/js/2.08b9fd1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2b84253bfb3ff4d33ba",
+    "url": "/olga/static/js/main.1d94278a.chunk.js"
+  },
+  {
+    "revision": "0e045de2868883d7d522",
+    "url": "/olga/static/js/runtime-main.b0ae316c.js"
+  },
+  {
+    "revision": "382870057febf0293896cceb27de5213",
+    "url": "/olga/static/media/desc.38287005.png"
+  },
+  {
+    "revision": "09edd1b62eb4001378bf9ed01f45b4ce",
+    "url": "/olga/static/media/main.09edd1b6.png"
+  },
+  {
+    "revision": "55dfb40d208cedd531a89126cacee3f3",
+    "url": "/olga/static/media/priceList.55dfb40d.jpg"
+  },
+  {
+    "revision": "f83b243afc4da93d29d2d87bd3e00a40",
+    "url": "/olga/static/media/slogan.f83b243a.png"
+  },
+  {
+    "revision": "647ff8339d1229ac888f76eacc8f6047",
+    "url": "/olga/static/media/viber.647ff833.png"
+  },
+  {
+    "revision": "6db49ff499b24eed8ee6be874d9cd090",
+    "url": "/olga/static/media/whatsup.6db49ff4.png"
+  }
+]);
